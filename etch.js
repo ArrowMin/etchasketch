5236952297b4ab743d6999f8956a84bc4a1dbd6a
arrowmin <aaromin@plmr.com>
2023-08-30 10:17:22 -0700

    implement rows and cells

--- etch.js
+++ etch.js
@@ -1,19 +1,19 @@
+const dimension = 16;
 
-
-function addElement() {
+function addRow() {
+    // Create Row
     const cellRow = document.createElement("div");
     cellRow.classList.add("cellRow")
     document.getElementById("mainDivContainer").appendChild(cellRow);
+    // Create Cell
+    for (let i = 0; i < dimension; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cellRow.appendChild(cell);
+    }
+
 }
 
-addElement();
-addElement();
-addElement();
-addElement();
-addElement();
-addElement();
-addElement();
-addElement();
-addElement();
-console.log("Connected")
-
+for (let i = 0; i < dimension; i++) {
+    addRow();
+}
